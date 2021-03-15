b42a0982a3172df5ef165022cbd32c20d504c838
Efflam <efflam.simon@etu.univ-nantes.fr>
2021-03-15 19:54:53 +0100

    Add projet with 1-page char table

---
+++ CharTable/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CharTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(244, 334);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "CharTable";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ CharTable/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharTable
+{
+    public partial class Form1 : Form
+    {
+        private const int padding = 13, margin = 7, size = 38, colnum = 5;
+
+        private string[] mainChars = { "À", "Ç", "ñ", "œ", "Œ", "ß" };
+        private Button[] charButtons;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            //
+            // Char Buttons
+            //
+            this.charButtons = new Button[mainChars.Length];
+            for (int i = 0; i < mainChars.Length; i++)
+            {
+                charButtons[i] = new Button();
+                Point logicLocation = new Point(i % colnum, i / colnum);
+                charButtons[i].Location = new Point(padding + (margin + size) * logicLocation.X, padding + (margin + size) * logicLocation.Y);
+                charButtons[i].Name = "charButton" + i;
+                charButtons[i].Size = new Size(size, size);
+                charButtons[i].TabIndex = 0;
+                charButtons[i].Text = mainChars[i];
+                charButtons[i].UseVisualStyleBackColor = true;
+                charButtons[i].Click += new EventHandler(charButton_Click);
+
+                Controls.Add(charButtons[i]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void charButton_Click(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                Clipboard.SetText(button.Text);
+                this.Close();
+            }
+        }
+    }
+}
